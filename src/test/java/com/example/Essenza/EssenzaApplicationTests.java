dd80fa5deb3f5871312058782b7f11f686991d6f
Diiegoof <teammax131@gmail.com>
2025-11-22 20:50:08 -0300

    Commit inicial por Spring Boot Backend

---
+++ src/test/java/com/example/Essenza/EssenzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Essenza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssenzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
